49d2156c551b86e03c6e1858b754891e33f2e11e
Zoltán Szabó <szabozoltan0108@gmail.com>
2026-04-30 21:41:09 +0200

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -33,10 +33,31 @@ function updateActive() {
     }
 }
 
+function changetheme() {
+    const root = document.documentElement;
+    root.classList.toggle("theme");
+
+    const img1 = document.getElementById("solo");
+    const img2 = document.getElementById("group");
+    const img3 = document.getElementById("kardio");
+    const img4 = document.getElementById("food");
+
+    if (root.classList.contains("theme")) {
+        img1.src = "pictures/egyeniedzesgold.png";
+        img2.src = "pictures/csoportosedzesgold.png";
+        img3.src = "pictures/kardiogold.png";
+        img4.src = "pictures/taplalkozasgold.png";     // arany verzió
+    } else {
+        img1.src = "pictures/egyeniedzes.png";
+        img2.src = "pictures/csoportosedzes.png";
+        img3.src = "pictures/kardio.png";
+        img4.src = "pictures/taplalkozas.png";  // alap verzió
+    }
+};
 
 /* js vége !!! bővítést csak feljebb */
 
 setMap(
-  "https://www.google.com/maps/embed?...",
+  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2697.3197027070355!2d19.0282250125388!3d47.464199097679156!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741ddbfaca09e79%3A0xc4c6ed726deabed2!2sChilli%20Fitness%20Budapest%20-%20Bik%C3%A1s%20park!5e0!3m2!1shu!2shu!4v1777549927616!5m2!1shu!2shu",
   false
 );
