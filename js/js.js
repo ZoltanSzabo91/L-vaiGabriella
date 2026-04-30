a6bda46687744b27a94d90791b40021e8a0079e8
Zoltán Szabó <szabozoltan0108@gmail.com>
2026-04-30 17:07:03 +0200

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -9,3 +9,34 @@ window.addEventListener("scroll", () => {
         menu.classList.remove("sticky-active");
     }
 });
+
+
+const c2 = document.getElementById("chilli2");
+const c5 = document.getElementById("chilli5");
+let maps = false;
+
+function setMap(address, state) {
+    const iframe = document.getElementById("map");
+    iframe.src = address;
+    maps = state;
+    updateActive();
+}
+
+function updateActive() {
+    if (maps) {
+        c2.classList.remove("active");
+        c5.classList.add("active");
+    }
+    else {
+        c2.classList.add("active");
+        c5.classList.remove("active");
+    }
+}
+
+
+/* js vége !!! bővítést csak feljebb */
+
+setMap(
+  "https://www.google.com/maps/embed?...",
+  false
+);
